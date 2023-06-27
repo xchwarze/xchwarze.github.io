6d1060cc80745667c82c09faea9cfd2fcc7544e1
DSR! <xchwarze@gmail.com>
2023-06-27 01:34:26 -0300

    Add old webpage code

---
+++ js/custom.js
@@ -0,0 +1,213 @@
+/* ----------------------------- 
+Pre Loader
+----------------------------- */
+$(window).load(function() {
+	'use strict';
+	$('.loading-icon').delay(500).fadeOut();
+	$('#preloader').delay(800).fadeOut('slow');
+});
+
+
+/* ----------------------------- 
+Backgroung slider
+----------------------------- */
+$(window).ready(function() {
+	'use strict';
+
+	var slideHeight = $(window).height();
+	$('.home-section-wrap').css('height',slideHeight);
+
+	$(window).resize(function(){'use strict',
+		$('.home-section-wrap').css('height', $(window).height());
+	});
+
+	var backgrounds = [
+		{ src:'images/bg-slider/bg-1-1400.jpg', fade:2000 },
+		{ src:'images/bg-slider/bg-2-1400.jpg', fade:2000 },
+		{ src:'images/bg-slider/bg-3-1400.jpg', fade:2000 }
+	];
+
+	$.vegas('slideshow', {
+	  backgrounds: shuffle(backgrounds)
+	})();
+});
+
+				
+
+/* ----------------------------- 
+Scroll into viewPort Animation
+----------------------------- */
+$(document).ready(function() {	
+	'use strict';
+	$('.animated').appear(function() {
+		var element = $(this),
+			animation = element.data('animation'),
+			animationDelay = element.data('animation-delay');
+			if ( animationDelay ) {
+
+				setTimeout(function(){
+					element.addClass( animation + " visible");
+				}, animationDelay);
+
+			} else {
+				element.addClass( animation + " visible");
+			}
+	});
+});
+	
+
+/* ----------------------------- 
+NiceScroll
+----------------------------- 	
+$(document).ready(function() { 
+	'use strict';
+  $("html").niceScroll({
+		cursorcolor: '#E74C3C',
+		cursoropacitymin: '1',
+		cursorborder: '0px',
+		cursorborderradius: '0px',
+		cursorwidth: '10px',
+		cursorminheight: 60,
+		horizrailenabled: false,
+		zindex: 1090,
+		passiveSupported: true,
+	});
+});*/
+
+
+/* ----------------------------- 
+IE 9 Placeholder fix
+----------------------------- */
+$('[placeholder]').focus(function() {
+  var input = $(this);
+  if (input.val() == input.attr('placeholder')) {
+    input.val('');
+    input.removeClass('placeholder');
+  }
+}).blur(function() {
+  var input = $(this);
+  if (input.val() == '' || input.val() == input.attr('placeholder')) {
+    input.addClass('placeholder');
+    input.val(input.attr('placeholder'));
+  }
+}).blur();
+
+
+
+/* ----------------------------- 
+Screenshot Load
+----------------------------- */	
+$(document).ready(function() {
+	'use strict';
+	$('.view-project').on('click', function(e) {
+		e.preventDefault();
+		
+		var href 			= $(this).attr('href') + ' .portfolio-project',
+			portfolioWrap	= $('.porfolio-container'),
+			contentLoaded 	= $('#portfolio-load'),
+			offset			= $('#section-screenshots').offset().top;
+		
+		portfolioWrap.animate({'left':'-120%'},{duration:400,queue:false});
+		portfolioWrap.fadeOut(400);
+		$('html, body').animate({scrollTop: offset},{duration:800,queue:true});
+		setTimeout(function(){ $('#portfolio-loader').fadeIn('fast'); },300);
+		
+		setTimeout(function(){
+            contentLoaded.load(href, function(){
+                $('#portfolio-loader').fadeOut('fast');
+                contentLoaded.fadeIn(600).animate({'left':'0'},{duration:800,queue:false});
+                $('.back-button').fadeIn(600);
+            });
+        },400);
+		
+		
+		
+	});
+	
+	$('.backToProject').on('click', function(e){
+		e.preventDefault();
+		
+		var portfolioWrap	= $('.porfolio-container'),
+			contentLoaded 	= $('#portfolio-load');
+			
+		contentLoaded.animate({'left':'105%'},{duration:400,queue:false}).delay(300).fadeOut(400);
+        $(this).parent().fadeOut(400);
+		setTimeout(function(){
+            portfolioWrap.animate({'left':'0'},{duration:400,queue:false});
+            portfolioWrap.fadeIn(600);
+        },500);
+		
+	});
+
+});
+
+				
+/* ----------------------------- 
+Main navigation
+----------------------------- */
+$(document).ready(function() {
+	'use strict';
+	$('.nav').onePageNav({
+		currentClass: 'current',
+		scrollSpeed: 1000,
+		easing: 'easeInOutQuint'
+	});
+	$(window).bind('scroll', function(e) {
+		var scrollPos = $(window).scrollTop();
+		scrollPos > 220 ? $('.sticky-section').addClass('nav-bg') : $('.sticky-section').removeClass('nav-bg');
+	});
+});
+
+			
+
+/* ----------------------------- 
+Contact form
+----------------------------- */			
+$(document).ready(function() {
+	'use strict';
+	$('form.contact-form').on('submit', function(e) {
+		$.post('contact.php', $(this).serialize(), function(response) {
+			if ($('.confirmation p').html() != "") {
+				$('.confirmation p').replaceWith('<p><span class="fa fa-check"></span></p>');
+			}
+			$('.confirmation p').append(response).parent('.confirmation').show();
+			$('html, body').animate({
+				scrollTop: $('#section-contact').offset().top
+				},{duration:800,queue:true});
+			$('.form-item').val('');
+			setTimeout(function() {
+				$('.confirmation').hide();
+			}, 8000);
+		});
+		// disable default action
+		e.preventDefault();
+	});
+
+});
+
+
+/**
+ * Randomly shuffle an array
+ * https://stackoverflow.com/a/2450976/1293256
+ * @param  {Array} array The array to shuffle
+ * @return {String}      The first item in the shuffled array
+ */
+var shuffle = function (array) {
+	var currentIndex = array.length;
+	var temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+};
+
